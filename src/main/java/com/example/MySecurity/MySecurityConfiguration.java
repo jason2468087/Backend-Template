b80ecf8b5fe79cd4bfaa4d000650b132b349880a
MSI\jason <jason.poon@lscm.hk>
2023-11-16 10:45:58 +0800

    Feat: Add basic Security Configuration

---
+++ src/main/java/com/example/MySecurity/MySecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.MySecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfiguration {
+	
+	// configure security properties
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http
+		// setup authorization of each page
+		.authorizeRequests() 
+			.requestMatchers("/welcome/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+		.httpBasic();
+		
+		
+		return http.build();
+	}
+}
